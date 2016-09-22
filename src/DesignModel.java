b8d6c6623e24a1e2f8949b89537d2186461ba7e7
hewenjie <172583172@qq.com>
2016-09-22 15:19:17 +0800

    Initial commit

---
+++ src/DesignModel.java
@@ -0,0 +1,17 @@
+/**
+ * Created by yiyuan on 2016/9/22.
+ */
+public class DesignModel {
+
+    String a = "ok";
+
+    public DesignModel() {
+        a = "q";
+    }
+
+    public static void main(String args[]) {
+        DesignModel d = new DesignModel();
+        System.out.print(d.a);
+    }
+
+}
