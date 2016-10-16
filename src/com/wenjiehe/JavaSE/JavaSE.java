39b8cf07896255dcb1e0f096974b4529cd44437b
hewenjie <172583172@qq.com>
2016-10-16 14:34:10 +0800

    集合 求素数个数

--- src/com/wenjiehe/JavaSE/JavaSE.java
+++ src/com/wenjiehe/JavaSE/JavaSE.java
@@ -4,9 +4,9 @@ package com.wenjiehe.JavaSE;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
+
+import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 
 
 /**
@@ -94,7 +94,7 @@ public class JavaSE implements Cloneable {
     }
 
     public static void main(String args[]) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, CloneNotSupportedException {
-        A a1 = new A();
+        /*A a1 = new A();
         A a2 = new B();
         //((B)a2).xx();
         B b = new B();
@@ -106,18 +106,18 @@ public class JavaSE implements Cloneable {
 
         A[] mm = new A[2];
         mm[0] = new B();
-        System.out.println(mm[0].show(mm[0]));
+        System.out.println(mm[0].show(mm[0]));*/
         /*Test cc =new Test();
         JavaSE js = cc;
         js=new JavaSE();
         System.out.println(cc.i);*/
 
-        String s1 = "nihao";
+        /*String s1 = "nihao";
         String s2 = "nihao";
         String s3 = new String("nihao");
         System.out.println(s1.equals(s2));    //    true
         System.out.println(s1.equals(s3));    //    false
-
+*/
 
         ArrayList<A> al = new ArrayList();
         // ((ArrayList<JavaSE>)b.arr()).get(0).replaceSpace(""));
@@ -133,7 +133,7 @@ public class JavaSE implements Cloneable {
         System.out.println(jse.xiaochengxu);
         xiaochengxu(jse);
         System.out.println(jse.xiaochengxu);*/
-
+/*
         String str1 = "3";
         Integer it = Integer.parseInt(str1);
         String str2 = it.toString();
@@ -181,7 +181,8 @@ public class JavaSE implements Cloneable {
 
 
         //ArrayList<Test> arraytest  = new ArrayList<>();
-        // ArrayList<JavaSE> arrayjavase = arraytest;
+         //ArrayList<JavaSE> arrayjavase = arraytest;
+        //arrayjavase.add(new Test());
         //编译器不允许
 
        // Pair<Test> pair1 = new Pair<>();
@@ -190,12 +191,69 @@ public class JavaSE implements Cloneable {
 
 
         Pair<? super Manager> pair = new Pair<Employee>(null, null);
-        minMaxSal(new Manager[] {new Manager("",9), new Manager("",8)}, pair);
+        minMaxSal(new Manager[] {new Manager("",9), new Manager("",8)}, pair);*/
+
+
+        List<String> a = new LinkedList<>();
+        a.add("a1");
+        a.add("a2");
+        a.add("a3");
+
+        List<String> b = new LinkedList<>();
+        b.add("b1");
+        b.add("b2");
+        b.add("b3");
+
+        ListIterator listIteratorA = a.listIterator();
+        Iterator IteratorB = b.iterator();
+
+        while (IteratorB.hasNext()) {
+            String mc = (String) IteratorB.next();
+            System.out.println(mc);
+            if (listIteratorA.hasNext())
+                listIteratorA.next();
+            listIteratorA.add(mc);
+        }
+
+        //listIteratorA.remove();
+        //listIteratorA.remove();
+
+        //System.out.println(a);
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("11");
+        ll.add("22");
+        ll.add("33");
+        ListIterator<String> li = (ListIterator<String>) ll.iterator();
+        li.next();
+        li.add("44");
+        while (li.hasNext()) {
+            System.out.println(li.next());
+            System.out.println("no next");
+        }
+        for (String s : ll)
+            System.out.println(s);
+        System.out.println();
+        ArrayList<String> all = new ArrayList();
+        all.add("11");
+        all.add("22");
+        all.add("33");
+        all.add("44");
+        //all.subList(0,3).clear();
+        String[] values = all.toArray(new String[0]);
+        Iterator<String> iterat = all.iterator();
+        while (iterat.hasNext())
+            System.out.println(iterat.next());
+        System.out.println(Integer.MAX_VALUE);
+        for(String s:values)
+            System.out.println(s);
+
+        System.out.println(sieve(100));
+        System.out.println("over");
     }
 
     public int xiaochengxu = 0;
 
-    static  void xiaochengxu(JavaSE javase) {
+    static void xiaochengxu(JavaSE javase) {
         Test t = new Test();
         //javase = new JavaSE();
         javase.xiaochengxu = 99;
@@ -221,6 +279,33 @@ public class JavaSE implements Cloneable {
         return (JavaSE) super.clone();
     }
 
+    public static  int sieve(int max){
+    BitSet bitSet = new BitSet(max+1);
+        int count =0;
+        int i=0;
+        for(i=2;i<=max;i++)
+            bitSet.set(i);
+
+        i=2;
+        while(i*i<=max){//对称
+            if(bitSet.get(i)){
+                count++;
+                int k = 2*i;
+                while(k<=max){
+                    bitSet.clear(k);
+                    k+=i;
+                }
+            }
+            i++;
+        }
+
+        while(i<=max){
+            if(bitSet.get(i))
+                count++;
+            i++;
+        }
+        return count;
+    }
 
     public static void minMaxSal(Manager[] mgrs, Pair<? super Manager> pair) {
         if (mgrs == null || mgrs.length == 0) {
