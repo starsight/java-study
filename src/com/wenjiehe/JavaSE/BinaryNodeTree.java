67e2e81b5f455497a9507fad6c81feaaf8231cf5
hewenjie <172583172@qq.com>
2016-10-19 23:19:57 +0800

    tree 4-20

--- src/com/wenjiehe/JavaSE/BinaryNodeTree.java
+++ src/com/wenjiehe/JavaSE/BinaryNodeTree.java
@@ -1,36 +1,103 @@
 package com.wenjiehe.JavaSE;
 
+import java.nio.BufferUnderflowException;
+import java.util.Comparator;
+
 /**
  * Created by yiyuan on 2016/10/19.
  */
-public class BinaryNodeTree <E extends Comparable<? super E>> {
-    private static class BinaryNode<E>{
+public class BinaryNodeTree<E> {
+    private static class BinaryNode<E> {
         E element;
         BinaryNode left;
         BinaryNode right;
 
-        BinaryNode(E e){
-            this(e,null,null);
+        BinaryNode(E e) {
+            this(e, null, null);
         }
 
-        BinaryNode(E e, BinaryNode left, BinaryNode right){
+        BinaryNode(E e, BinaryNode left, BinaryNode right) {
             this.element = e;
             this.left = left;
             this.right = right;
         }
     }
 
-    BinaryNode root;
-    private boolean contain(E e,BinaryNode<E> p){
-        if(p==null)
-            return false;
-        int comp = e.compareTo(p.element);
+    private BinaryNode<E> root;
+    private Comparator<? super E> cmp;
 
-        if(comp>0)
-            return  contain(e,p.right);
-        else if(comp<0)
-            return contain(e,p.left);
+
+    public BinaryNodeTree() {
+        this(null);
+    }
+
+    BinaryNodeTree(Comparator c) {
+        this.cmp = c;
+        this.root = null;
+    }
+
+    private int myCompare(E e1, E e2) {
+        if (cmp == null)
+            return ((Comparable) e1).compareTo(e2);
+        else
+            return cmp.compare(e1,e2);
+
+    }
+
+    public void makeEmpty() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean contain(E e) {
+        return contain(e, root);
+    }
+
+    private boolean contain(E e, BinaryNode<E> p) {
+        if (p == null)
+            return false;
+        int comp = myCompare(e, p.element);
+
+        if (comp > 0)
+            return contain(e, p.right);
+        else if (comp < 0)
+            return contain(e, p.left);
         else
             return true;
+    }
+
+    public E findMin(){
+        if(isEmpty())
+            throw new NullPointerException();
+        else
+            return findMin(root).element;
+    }
+
+    private BinaryNode<E> findMin(BinaryNode bn){
+        if(bn ==null)
+            return null;
+        else if(bn.left==null)
+            return bn;
+        return findMin(bn.left);
+    }
+
+
+
+    public E findMax(){
+        if(isEmpty())
+            throw new NullPointerException();
+        else
+            return findMax(root).element;
+    }
+
+    private BinaryNode<E> findMax(BinaryNode<E> bn){
+        if(bn ==null)
+            return null;
+        else if(bn.right==null)
+            return bn;
+        return findMax(bn.right);
     }
 }
