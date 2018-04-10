516cb922bc9e6aee0141e2b1817cd7b63eb4361b
starsight <172583172@qq.com>
2018-04-10 23:15:18 +0800

    Huawei实习生测试题,第三题改成自己的方法

--- src/com/wenjiehe/NowCoder/Huawei03.java
+++ src/com/wenjiehe/NowCoder/Huawei03.java
@@ -5,6 +5,7 @@ public class Huawei03 {
         String a="1234";
         String b="4321";
         String c=test1(a.toCharArray(),b.toCharArray());
+        //String c=test3(a.toCharArray(),b.toCharArray(),2);
         System.out.println(c);
     }
     public static String test1(char[] k, char[] l) {
@@ -48,7 +49,55 @@ public class Huawei03 {
         return value;
     }
     public static String test3(char[] k2, char[] l3, int l) {
-        int maxlen = k2.length + l3.length;
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+        for (int i = 0; i < k2.length; i++) {
+            sb1.append(k2[i]);
+        }
+        for (int i = 0; i < l3.length; i++) {
+            sb2.append(l3[i]);
+        }
+        for (int i = 0; i < l; i++) {
+            sb2.append("0");
+        }
+
+        k2 =sb1.reverse().toString().toCharArray();
+        l3 =sb2.reverse().toString().toCharArray();
+
+        int p = 0;
+        String out = "";
+        int minLen = k2.length>l3.length?l3.length:k2.length;
+        int maxLen =minLen==k2.length?l3.length:k2.length;
+        char[] maxChar = minLen==k2.length?l3:k2;
+
+        for (int i = 0; i <minLen ; i++) {
+            int temp = k2[i] - '0';
+            int tempb = l3[i] - '0';
+            int sum = temp+tempb+p;
+            p = sum / 10;
+            int left = sum % 10;
+            out += left;
+        }
+
+
+        for (int i = minLen; i <maxLen ; i++) {
+            if(p==1){
+                int x = maxChar[i]-'0'+1;
+                out+=x;
+                p=0;
+            }else{
+                out+=maxChar[i];
+            }
+        }
+
+        char[] sb = out.toCharArray();
+        String value = "";
+        for (int j = sb.length - 1; j >= 0; j--)
+            value += sb[j];
+
+        return value;
+
+        /*int maxlen = k2.length + l3.length;
         char[] revA = test4(k2);
         char[] revB = test4(l3);
         String sb = "";
@@ -78,7 +127,7 @@ public class Huawei03 {
         String value = "";
         for (int j = result.length - 1; j >= 0; j--)
             value += result[j];
-        return value;
+        return value;*/
     }
     public static char[] test4(char[] h) {
         char[] p = new char[h.length];
