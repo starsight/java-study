1fc14b09adfedf82d86d3729b8b6185bae464d80
starsight <172583172@qq.com>
2018-08-11 20:34:38 +0800

    网易秋招提前批

---
+++ src/com/wenjiehe/QiuZhao/Netease/Netease0811_01.java
@@ -0,0 +1,75 @@
+package com.wenjiehe.QiuZhao.Netease;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2018/8/11.
+ */
+public class Netease0811_01 {
+    //0%
+    public static void main(String[] args) {
+        solution();
+    }
+    public static void solution(){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int k = sc.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
+//        String s1 = sc.nextLine();
+//        String[] ss1 = s1.split(" ");
+//        int n = Integer.valueOf(ss1[0]);
+//        int k = Integer.valueOf(ss1[1]);
+//        Integer[] arr = new Integer[n];
+//        s1 = sc.nextLine();
+//        ss1 = s1.split(" ");
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = Integer.valueOf(ss1[i]);
+//        }
+
+        int count =k;
+        int[] res;
+        String[] str = new String[k];
+        for (int i = 0; i < k; i++) {
+            res = maxmin(arr);
+            if(res[0]==res[1]){
+                count =i;
+                break;
+            }
+            arr[res[0]]--;
+            arr[res[1]]++;
+            res[0]++;
+            res[1]++;
+            str[i] = res[0]+" "+res[1];
+        }
+
+        res= maxmin(arr);
+        System.out.println(res[0]-res[1]+" "+count);
+        for (int i = 0; i < count; i++) {
+            System.out.println(str[i]);
+        }
+
+    }
+
+    public static int[] maxmin(Integer[] arr){
+        int[] res = new int[2];
+        int max =Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(max<arr[i]){
+                max = arr[i];
+                res[0] =i;
+            }
+            if(min>arr[i]){
+                min = arr[i];
+                res[1] =i;
+            }
+        }
+        return res;
+    }
+
+
+}
---
+++ src/com/wenjiehe/QiuZhao/Netease/Netease0811_02.java
@@ -0,0 +1,64 @@
+package com.wenjiehe.QiuZhao.Netease;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator on 2018/8/11.
+ */
+public class Netease0811_02 {
+    // 60%
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static void solution() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] apple = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            apple[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int result[] = new int[m];
+        for (int i = 0; i < m; i++) {
+            result[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            count += apple[i];
+            apple[i] = count;
+        }
+
+        int index = 1;
+
+        for (int i = 0; i < m; i++) {
+            index = binarySearch(apple, result[i]);
+            System.out.println(index + 1);
+
+        }
+
+    }
+
+    public static int binarySearch(int[] arr, int des) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (middle == 0) {
+                return 0;
+            }
+            if (arr[middle] >= des && arr[middle - 1] < des) {
+                //index =middle;
+                return middle;
+            } else if (arr[middle] < des) {
+                low = middle + 1;
+            } else {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
